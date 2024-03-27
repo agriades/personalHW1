2438e6330346ec2eb4ed457707952472d80cc351
Sooyoung <agriades23@gmail.com>
2024-03-27 15:57:23 +0900

    레벨 1 완료: 로고 빼고

---
+++ app/src/main/java/com/homework/account/SignInActivity.kt
@@ -0,0 +1,38 @@
+package com.homework.account
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+
+        val loginButton = findViewById<Button>(R.id.button_login)
+        loginButton.setOnClickListener{
+            val textID = findViewById<EditText>(R.id.edit_text_id).text.toString()
+            val textPW = findViewById<EditText>(R.id.edit_text_pw).text.toString()
+
+            if (textID == "" || textPW == "") {
+                Toast.makeText(this.applicationContext, "아이디/비밀번호를 확인해 주세요.", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                val homeIntent = Intent(this, HomeActivity::class.java)
+                homeIntent.putExtra("userID",textID)
+                homeIntent.putExtra("userPW",textPW)
+                Toast.makeText(this.applicationContext, "로그인 성공", Toast.LENGTH_SHORT).show()
+                startActivity(homeIntent)
+            }
+        }
+        val signUpButton = findViewById<Button>(R.id.button_sign_up)
+        signUpButton.setOnClickListener{
+            val signUpIntent = Intent(this, SignUpActivity::class.java)
+            startActivity(signUpIntent)
+        }
+
+    }
+}
---
+++ app/src/main/java/com/homework/account/SignUpActivity.kt
@@ -0,0 +1,15 @@
+package com.homework.account
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
