eb175a118c339c89e650e1bbe15b8bdf23081fee
Sooyoung <agriades23@gmail.com>
2024-03-27 16:23:55 +0900

    레벨 2까지, 이미지 추가 및 레이아웃 다듬기  빼고 완료.

--- app/src/main/java/com/homework/account/SignUpActivity.kt
+++ app/src/main/java/com/homework/account/SignUpActivity.kt
@@ -1,7 +1,10 @@
 package com.homework.account
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,5 +14,20 @@ class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        val signUpButton = findViewById<Button>(R.id.button_sign_up)
+        signUpButton.setOnClickListener {
+            val textName = findViewById<EditText>(R.id.edit_text_name).text.toString()
+            val textID = findViewById<EditText>(R.id.edit_text_id).text.toString()
+            val textPW = findViewById<EditText>(R.id.edit_text_pw).text.toString()
+
+            if (textName == "" || textID == "" || textPW == "") {
+                Toast.makeText(this.applicationContext, "입력되지 않은 정보가 있습니다.", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                finish()
+            }
+        }
     }
+
 }
